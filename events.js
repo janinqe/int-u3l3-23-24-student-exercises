8c92b057c5005ed9e2837c6dc9f49782e2ff449f
janinqe <158521469+janinqe@users.noreply.github.com>
2024-04-11 21:05:17 +0000

    done

--- events.js
+++ events.js
@@ -13,14 +13,16 @@ let growButton = document.querySelector(".grow");
 
 /* 1. When jump button is clicked, Mario jumps and coin disappears.  
 
-________.addEventListener("click", function() {
+_______.addEventListener("click", function() {
     mario.style.bottom = "120px";
     coin.style.display = "none";
 });
 
 */
-
-
+jumpButton.addEventListener("click", function() {
+mario.style.bottom = "120px";
+coin.style.display = "none";
+  });
 /* 2. When jump down button is clicked, Mario jumps down. 
 
 ________.addEventListener("click", function() {
@@ -28,8 +30,9 @@ ________.addEventListener("click", function() {
 });
 
 */
-
-
+jumpDownButton.addEventListener("click", function() {
+  mario.style = "bottom: 35px";
+  });
 /* 3. When change color button is clicked, background color changes. 
 
 ________.addEventListener("click", function() {
@@ -37,7 +40,9 @@ ________.addEventListener("click", function() {
 });
 
 */
-
+changeColorButton.addEventListener("click", function() {background.style.backgroundColor = "pink"
+  // you can change the color to something different
+});
 
 /* 4. When end game button is clicked, the title of the page says game over. 
 
@@ -46,6 +51,9 @@ ________._______ ("___", ________() {
 });
 
 */
+endGameButton.addEventListener ("click", function() {
+    title.innerHTML = "Game over.";
+});
 
 
 /* 5. When update score button is clicked, the counter score becomes "1" instead of 0. 
@@ -55,12 +63,14 @@ ________._______ ("___", ________() {
 });
 
 */
+updateScoreButton.addEventListener ("click", function() {
+    score.innerHTML = 1;
+});
 
 
 /* 6: When clicking the "Grow" button the Mario image gets bigger. Write the whole click handler! 
-
-{
-    mario.style.width = "200px";
-}
-
 */
+growButton.addEventListener ("click", function() {
+mario.style.width = "1000px";
+})
+
